d8ecb6ea3eb156431e99a16ae20cfc99b80b51fa
MICHGIL <50332018+MICHGIL@users.noreply.github.com>
2019-06-11 02:25:51 +0200

    Add files via upload

--- rotate.py
+++ rotate.py
@@ -1,12 +1,23 @@
+from __future__ import print_function
+import PIL
+import os, sys
+from PIL import Image, ImageTk
+from colorama import init
+from termcolor import colored
+from pyfiglet import Figlet
+from tkinter import *
+from tkinter import messagebox
+init()
+
 x="obraz.jpg"
 def rotate90():
-    im= Image.open(x).rotate(90) #rotate by 90 degrees
+    im= PIL.Image.open(x).rotate(90) #rotate by 90 degrees
     im.show()
 def rotate180():
-    im= Image.open(x).rotate(180) #rotate by 180 degrees
+    im= PIL.Image.open(x).rotate(180) #rotate by 180 degrees
     im.show()
 def rotate270():
-    im= Image.open(x).rotate(270) #roatate by 270 degrees
+    im= PIL.Image.open(x).rotate(270) #roatate by 270 degrees
     im.show()
 def akcja():
     messagebox.showinfo("O ile stopni chcesz obrócić obrazek?", pole_tekstowe()) #question abot rotation
@@ -17,7 +28,7 @@ glowne_okno=Tk()
 glowne_okno.title("rotate")
 plotno=Canvas(glowne_okno, width=w, height=h)
 plotno.pack()
-obraz=Image.open("obraz.jpg")
+obraz=PIL.Image.open("obraz.jpg")
 obrazTk=ImageTk.PhotoImage(obraz)
 plotno.create_image(w/2,h/2, image=obrazTk)
 opis_pola_tekstowego= Label(glowne_okno, text= "O ile stopni chcesz obrócić obrazek?")
